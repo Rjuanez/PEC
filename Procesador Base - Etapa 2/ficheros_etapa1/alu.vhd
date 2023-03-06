153ca0739fd7efbfc8d24123e38f5cb2661a763a
victor.mena <victor.mena@estudiantat.upc.edu>
2023-03-06 23:46:57 +0100

    Second Phase

---
+++ Procesador Base - Etapa 2/ficheros_etapa1/alu.vhd
@@ -0,0 +1,21 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+
+ENTITY alu IS
+    PORT (x  : IN  STD_LOGIC_VECTOR(15 DOWNTO 0);
+          y  : IN  STD_LOGIC_VECTOR(15 DOWNTO 0);
+          op : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+          w  : OUT STD_LOGIC_VECTOR(15 DOWNTO 0));
+END alu;
+
+
+ARCHITECTURE Structure OF alu IS
+BEGIN
+	
+	with op select
+		w <= y when "00", --MOVI
+			  (y(7 DOWNTO 0) & x(7 DOWNTO 0)) when "01", --MOVHI
+			  x+y when others; --SUMA
+
+END Structure;
